00cbc55348632fc042aaac875fcc8f1fec743e32
Yoland Yan <yoland68@gmail.com>
2015-02-19 16:09:15 -0500

    database stuff

--- pytutorial/settings.py
+++ pytutorial/settings.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
+
 BASE_DIR = os.path.dirname(__file__)
 TEMPLATE_PATH = os.path.join(BASE_DIR, "templates")
 
@@ -66,12 +68,7 @@ WSGI_APPLICATION = 'pytutorial.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {'default':dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
