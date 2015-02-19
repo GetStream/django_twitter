3d7b798983b8b00e603aecac5fa211ea9a5c4bf6
Yoland Yan <yoland68@gmail.com>
2015-02-19 12:40:45 -0500

    turn on debugging not by env in app.json

--- pytutorial/settings.py
+++ pytutorial/settings.py
@@ -21,9 +21,12 @@ TEMPLATE_PATH = os.path.join(BASE_DIR, "templates")
 SECRET_KEY = '*m&(&5!c^7j^7s$33u(bt567k!q0)@&p1io_w($ec+g66zr!0@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "off") == "on" 
 
-TEMPLATE_DEBUG = os.environ.get("DEBUG", "off") == "on"
+# DEBUG = os.environ.get("DEBUG", "off") == "on" 
+DEBUG = True
+
+# TEMPLATE_DEBUG = os.environ.get("DEBUG", "off") == "on"
+TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
