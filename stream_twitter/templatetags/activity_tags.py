3122f9edb0664777c62b8590ca1267efc6efafba
Yoland Yan <yoland68@gmail.com>
2015-02-24 13:54:45 -0500

    front end all

--- stream_twitter/templatetags/activity_tags.py
+++ stream_twitter/templatetags/activity_tags.py
@@ -4,7 +4,9 @@ register = template.Library()
 
 @register.simple_tag
 def render_activity(activity):
-    user = '<span class="user">{0}</span>'.format(activity.get(u'actor')
+    user = '<span class="user">{0}</span>'.format(activity.get(u'actor'))
     verb = '<span class="verb">{0}</span>'.format(activity.get(u'verb'))
-    statement = '{0} {1}: {2}'.format(user, verb, activity.get(u'object').text)
+    statement = '<div class="activity">{0} {1}: \
+        <span class="activity-text">{2}</span></div>'\
+        .format(user, verb, activity.get(u'object').text)
     return statement
