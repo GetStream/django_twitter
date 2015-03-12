1bcd0fc703fcf52e2d0cb5023f1f77639c93c9e8
Tommaso Barbugli <tbarbugli@gmail.com>
2015-03-12 17:47:54 -0400

    enable feed manager

--- pytutorial/management/commands/after_deploy.py
+++ pytutorial/management/commands/after_deploy.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
 from stream_twitter.models import Tweet
+from stream_django.feed_manager import feed_manager
 
 
 class Command(BaseCommand):
@@ -8,6 +9,9 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         call_command('migrate', interactive=False)
         call_command('loaddata', './pytutorial/fixtures/initial_data.json')
+
+        #make sure we send data to Stream
+        feed_manager.enable_model_tracking()
 
         for tweet in Tweet.objects.all():
             tweet.delete()
