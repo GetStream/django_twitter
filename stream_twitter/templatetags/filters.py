a59d989df6e131baedbdd38594f0bdf0e50c3b55
CMYK_Y <yoland68@users.noreply.github.com>
2015-03-25 10:46:08 -0400

    Update filters.py

--- stream_twitter/templatetags/filters.py
+++ stream_twitter/templatetags/filters.py
@@ -1,8 +1,23 @@
 from django import template
 from django.utils.html import format_html, escape
+from django.utils.timesince import timesince
+from datetime import *
+from django.utils.timezone import utc
+
 
 register = template.Library()
 
+@register.filter(name='time_difference')
+def time_difference(value):
+    now = datetime.utcnow().replace(tzinfo=utc)
+    try:
+        difference = now - value
+    except:
+        return value
+
+    if difference <= timedelta(minutes=1):
+        return 'Just now'
+    return '%(time)s ago' % {'time': timesince(value).split(', ')[0]}
 
 @register.filter(name='parse_tweet_text')
 def parse_tweet_text(tweet_object, autoescape=False):
