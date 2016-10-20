4f64c9101b9c149d1dad3f3de23bd6d63b6c9f68
Jelte Fennema <github-tech@jeltef.nl>
2016-10-20 15:26:19 +0200

    Use template.context_processors

--- pytutorial/settings.py
+++ pytutorial/settings.py
@@ -71,13 +71,13 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.core.context_processors.request',
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.core.context_processors.debug',
-                'django.core.context_processors.i18n',
-                'django.core.context_processors.media',
-                'django.core.context_processors.static',
-                'django.core.context_processors.tz',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
                 'django.contrib.messages.context_processors.messages'
             ],
         },
