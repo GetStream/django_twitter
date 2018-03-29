9f40b8740d183a473bd9fa0ffd8fad24ee2fba26
Dwight Gunning <dwight@dwightgunning.com>
2018-03-29 17:04:33 +1000

    Updated to django class based views

--- pytutorial/urls.py
+++ pytutorial/urls.py
@@ -1,33 +1,34 @@
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
-from stream_twitter import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf import settings
+from django.urls import path, re_path
 from django.views.generic import TemplateView
 from django.views.static import serve
 
-# from allauth.account import urls as allauthurls
+from stream_twitter import views
 
 urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
-    url(r'^admin/', admin.site.urls),
-    url(r'^discover/', login_required(views.discover)),
-    url(r'^timeline/',
-        login_required(views.TimelineView.as_view())),
-    url(r'^user/(?P<user_name>.+)/$', views.user),
-    url(r'^hashtag/(?P<hashtag_name>.+)/', views.hashtag),
-    url(r'^$', views.HomeView.as_view()),
-    url(r'^follow/$', login_required(views.follow), name='follow'),
-    url(r'^unfollow/(?P<target_id>\d+)/$', login_required(views.unfollow),
+    path('admin/', admin.site.urls),
+    path('discover/', login_required(views.DiscoverView.as_view()), name='discover'),
+    path('timeline/',
+        login_required(views.TimelineView.as_view()), name='timeline_feed'),
+    re_path(r'^user/(?P<username>.+)/', views.UserView.as_view(), name='user_feed'),
+
+    re_path(r'^hashtag/(?P<hashtag>.+)/', views.HashtagView.as_view(), name='hashtag_feed'),
+    path('', views.HomeView.as_view()),
+    path('follow/', login_required(views.FollowView.as_view()), name='follow'),
+    re_path(r'^unfollow/(?P<target_id>\d+)/', login_required(views.UnfollowView.as_view()),
         name='unfollow'),
-    url(r'^robots.txt$', TemplateView.as_view(
+    path('robots.txt', TemplateView.as_view(
       template_name='stream_twitter/robots.txt',
       content_type="text/plain"))
 ]
 
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += [
-    url(r'^media/(?P<path>.*)', serve,
+    re_path('^media/(?P<path>.*)', serve,
         {'document_root': settings.MEDIA_ROOT})
 ]
